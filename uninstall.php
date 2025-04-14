2d5566712feef5ed1fc5facc4d7f66925b3d14ea
Marco Buttarini <webgrafia@gmail.com>
2025-04-14 23:23:42 +0200

    Add uninstall handler for Cheshire Cat WP plugin
    Includes a script to clean up plugin data during uninstallation. Deletes specific options from the database to ensure no residual data remains.

---
+++ uninstall.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Fired when the Cheshire Cat WP plugin is uninstalled.
+ *
+ * This file is responsible for cleaning up plugin data from the database.
+ *
+ * @package   CheshireCatWp
+ * @since     0.4.0 // Or the version you add this in
+ */
+
+// If uninstall not called from WordPress, then exit.
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+    exit;
+}
+
+
+$option_names = array(
+    'cheshire_plugin_url',
+    'cheshire_plugin_token',
+    'cheshire_plugin_global_chat',
+    'cheshire_chat_background_color',
+    'cheshire_chat_text_color',
+    'cheshire_chat_user_message_color',
+    'cheshire_chat_bot_message_color',
+    'cheshire_chat_button_color',
+    'cheshire_chat_font_family',
+    'cheshire_chat_welcome_message'
+);
+
+// Loop through the option names and delete them from the database.
+foreach ( $option_names as $option_name ) {
+    if ( ! empty( $option_name ) ) {
+        delete_option( $option_name );
+
+    }
+}
+
+?>
